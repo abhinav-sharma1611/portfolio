5b6fd06c59fe4894c3c53c676aeab41ba400de79
eventsinc <abhinav.sharma@doxa-holdings.com>
2022-09-04 19:19:49 +0530

    Projects updated

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -16,7 +16,7 @@ const Hero = (props) => (
           I'm ABHINAV SHARMA
         </SectionTitle>
         <SectionText>
-          A passionate Web Developer (ReactJS)  having 3.10yrs experience of building Web applications with JavaScript / Reactjs  / Bootstrap / GraphQL / Redux / HTML / CSS / NextJS and some other cool libraries and frameworks.
+          A passionate Web Developer (ReactJS)  having 4.10yrs experience of building Web applications with JavaScript / Reactjs  / Bootstrap / GraphQL / Redux / HTML / CSS / NextJS and some other cool libraries and frameworks.
         </SectionText>
         <a download href="/pdf/Abhinav-ReactJS-Resume-3.10.pdf" target="_blank"><Button><SocialIcons >
           <AiOutlineCloudDownload size="4rem" />
